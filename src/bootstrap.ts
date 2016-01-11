fcd0b45be5366b58bb959a9c39896ca2e070c64b
mattimatti <mmonti@gmail.com>
2016-01-11 21:15:18 +0100

    Login example

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,9 +2,15 @@ import { bootstrap } from 'angular2/platform/browser';
 import { ROUTER_PROVIDERS } from 'angular2/router';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import { App } from './components/app/app';
+
+
+// Add these symbols to override the `LocationStrategy`
+import {provide}           from 'angular2/core';
+
+import {LocationStrategy,HashLocationStrategy} from 'angular2/router';
 // import {enableProdMode} from 'angular2/core';
 
 bootstrap(App, [
-  ROUTER_PROVIDERS,
-  HTTP_PROVIDERS
+	ROUTER_PROVIDERS,
+	HTTP_PROVIDERS
 ]);
--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,11 +1,49 @@
-import { Component } from 'angular2/core';
+import { Component, View } from 'angular2/core';
+import { Login } from '../login/login';
+import { Home } from '../home/home';
+import {OnInit} from 'angular2/core';
+import { RouterOutlet } from 'angular2/router';
+
+import {Location, RouteConfig, RouterLink, Router} from 'angular2/router';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {User} from '../../models/User';
+
+import {LoggedInRouterOutlet} from './LoggedInRouterOutlet';
+
+
+
 
 @Component({
-  selector: 'app',
-  host: { class: 'app' },
-  templateUrl: './dist/components/app/app.html'
+  selector: 'app'
 })
+
+@View({
+	templateUrl: './dist/components/app/app.html',
+	directives: [LoggedInRouterOutlet]
+})
+
+
+@RouteConfig([
+	{ path: '/', redirectTo: ['/Home'] },
+	{path: '/home',   name: 'Home',  component: Home },
+	{ path: '/login', name: 'Login', component: Login }
+])
 
 export class App {
 
+
+	/**
+	 * [constructor description]
+	 * @param {LoginService} private _loginService The injected service
+	 */
+	constructor(
+		public router: Router
+	) { }
+
+
+
+
 }
+
+
